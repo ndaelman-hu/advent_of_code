8d4084a69362973f928d20f6bcc37a2f2047f04e
Nathan Daelman <107392603+ndaelman-hu@users.noreply.github.com>
2025-02-28 18:11:53 +0100

    Add parsing to solution day 7 2024
    - TODO: extract example to separate file

--- 2024/day_7/solution.hs
+++ 2024/day_7/solution.hs
@@ -1,16 +1,14 @@
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
 main :: IO ()
-main = (putStrLn . show . sum) $
-  (\(v, is) -> selForSum (deconstruct v is) v) <$>
-  [(190, [10, 19])
-  ,(3267, [81, 40, 27])
-  ,(83, [17, 5])
-  ,(156, [15, 6])
-  ,(7290, [6, 8, 6, 15])
-  ,(161011, [16, 10, 13])
-  ,(192, [17, 8, 14])
-  ,(21037, [9, 7, 18, 13])
-  ,(292, [11, 6, 16, 20])]
-  
+main = let b = "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20\n"
+  in case parse body "" b of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right result -> putStrLn . show . sum $
+      (\(v, is) -> selForSum (deconstruct v is) v) <$> result
+
+-- solution logic
 selForSum :: (Int, String) -> Int -> Int
 selForSum res v = if fst res == 0 then v else 0
 
@@ -28,3 +26,13 @@ decide v i =
   _  | diff == 0 -> (diff, '+')
      | v `mod` i == 0 -> (v `div` i, '*')
      | otherwise -> (diff, '+')
+
+-- parsing logic
+body :: Parser [(Int, [Int])]
+body = many (line <* newline) <* eof
+
+line :: Parser (Int, [Int])
+line = (,) <$> digits <* char ':' <*> many (char ' ' *> digits) -- beware: char over space
+
+digits :: Parser Int
+digits = read <$> many1 digit
