30d58cea83747f4ed30a13f36cf33684efd7a9ba
Nathan Daelman <107392603+ndaelman-hu@users.noreply.github.com>
2025-03-04 20:51:53 +0100

    Change day 14 2024 to use `Data.Vector`

--- 2024/day_14/solution.hs
+++ 2024/day_14/solution.hs
@@ -1,58 +1,56 @@
 import Data.Function
 import Data.List
+import qualified Data.Vector as V
 import Text.Parsec
 import Text.Parsec.String (Parser)
+-- Data.Vector.Sized ?
 
 main :: IO ()
 main = let input="p=0,4 v=3,-3\np=6,3 v=-1,-3\np=10,3 v=-1,2\np=2,0 v=2,-1\np=0,0 v=1,3\np=3,0 v=-2,-2\np=7,6 v=-1,-3\np=3,0 v=-1,-2\np=9,3 v=2,3\np=7,3 v=-1,2\np=2,4 v=2,-3\np=9,5 v=-3,-3"
   in case parse (many1 line <* eof) "" input of
     Left err -> putStrLn . show $ err
-    Right res -> print . innerMultQ $ foldl' (addQ) zeroQ ((inQ 51 52) . uncurry (propogate 101 103 100) <$> res)
+    Right res -> print . innerMultQ $ foldl' (addV) zeroQ ((inQ 51 52) . uncurry (propogate 101 103 100) <$> res)
 
-newtype Quadr = Quadr (Int, Int, Int, Int)
+zeroQ = V.replicate 4 0
 
-zeroQ = Quadr (0, 0, 0, 0)
+setOne :: Int -> V.Vector Int
+setOne i = zeroQ V.// [(i, 1)] 
 
-innerMultQ :: Quadr -> Int
-innerMultQ (Quadr (x, y, z, u)) = x * y * z * u
+innerMultQ :: V.Vector Int -> Int
+innerMultQ = V.foldl' (*) 1
 
-inQ :: Int -> Int -> Coord -> Quadr
-inQ mx my (Coord (x, y))
+inQ :: Int -> Int -> V.Vector Int -> V.Vector Int
+inQ mx my v
   | x == mx = zeroQ
   | y == my = zeroQ
-  | x < mx && y < my = Quadr (1, 0, 0, 0)
-  | x > mx && y < my = Quadr (0, 1, 0, 0)
-  | x < mx && y > my = Quadr (0, 0, 1, 0)
-  | x > mx && y > my = Quadr (0, 0, 0, 1)
-
-addQ :: Quadr -> Quadr -> Quadr
-addQ (Quadr (x1, y1, z1, u1)) (Quadr (x2, y2, z2, u2)) = Quadr (x1 + x2, y1 + y2, z1 + z2, u1 + u2)
-
-newtype Coord = Coord (Int, Int)
-  deriving (Show, Read)
+  | x < mx && y < my = setOne 0
+  | x > mx && y < my = setOne 1
+  | x < mx && y > my = setOne 2
+  | x > mx && y > my = setOne 3
+    where x = v V.! 0; y = v V.! 1
   
-propogate :: Int -> Int -> Int -> Coord -> Coord -> Coord
-propogate mx my t p v = modC mx my $ addC p $ scalC t v
+propogate :: Int -> Int -> Int -> V.Vector Int -> V.Vector Int -> V.Vector Int
+propogate mx my t p v = modC mx my $ addV p $ scalC t v
   
-addC :: Coord -> Coord -> Coord
-addC (Coord (x1, y1)) (Coord (x2, y2)) = Coord (x1 + x2, y1 + y2)
+addV :: V.Vector Int -> V.Vector Int -> V.Vector Int
+addV = V.zipWith (+)
 
-scalC :: Int -> Coord -> Coord
-scalC i (Coord (x, y)) = Coord (i * x, i * y)
+scalC :: Int -> V.Vector Int -> V.Vector Int
+scalC = fmap . (*)
 
-modC :: Int -> Int -> Coord -> Coord
-modC mx my (Coord (x, y)) = Coord (x `mod` mx, y `mod` my)
+modC :: Int -> Int -> V.Vector Int -> V.Vector Int
+modC mx my v = V.fromList $ map (\x -> mod (v V.! (fst x)) (snd x)) (zip [0..] [mx, my])
 
 -- parsing
 
-line :: Parser (Coord, Coord) -- e.g. "p=0,4 v=3,-3"
+line :: Parser (V.Vector Int, V.Vector Int) -- e.g. "p=0,4 v=3,-3"
 line = (,) <$> (position <* char ' ') <*> (velocity <* optional newline)
 
-position :: Parser Coord
-position = Coord <$> ((,) <$> (read <$> (string "p=" *> many1 digit)) <*> (read <$> (char ',' *> many1 digit)))
+position :: Parser (V.Vector Int)
+position = V.fromList <$> sequence [read <$> (string "p=" *> many1 digit), read <$> (char ',' *> many1 digit)]
 
-velocity :: Parser Coord
-velocity = Coord <$> ((,) <$> (string "v=" *> negdigs) <*> (char ',' *> negdigs))
+velocity :: Parser (V.Vector Int)
+velocity = V.fromList <$> sequence [string "v=" *> negdigs, char ',' *> negdigs]
 
 negdigs :: Parser Int
 negdigs = (*) <$> (option 1 (char '-' >> return (-1))) <*> (read <$> many1 digit)
