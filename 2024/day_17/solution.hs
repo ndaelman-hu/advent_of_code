496abd54db3fb8b980eba136d45c5b840ab8dd66
Nathan Daelman <107392603+ndaelman-hu@users.noreply.github.com>
2025-03-09 22:57:38 +0100

    Update solution.hs day 17 2024
    Change type signature for basic moves to `[Int] -> State Reg [Int]` to consume instruction set.

--- 2024/day_17/solution.hs
+++ 2024/day_17/solution.hs
@@ -3,54 +3,57 @@ import Data.Bits (xor)
 
 main :: IO ()
 
-adv :: Int -> State Reg () -- opcode 0
+adv :: [Int] -> State Reg [Int] -- opcode 0
 adv = dv A
 
-bxl :: Int -> State Reg () -- opcode 1
-bxl literal = do
-  state <- get
-  newReg = B state `xor` literal
-  put (state {B = newReg})
+bxl :: [Int] -> State Reg [Int] -- opcode 1
+bxl literals = bx (literals !! 1)
 
-bst :: Int -> State Reg () -- opcode 2
-bst combo = do
+bst :: [Int] -> State Reg [Int] -- opcode 2
+bst _ = do
   state <- get
   newReg = (B state) % 8
   put (state {B = newReg})  -- update B register
-  return
+  jmp 2
 
-jnz :: Int -> State Reg () -- opcode 3
-jnz literal = do
+jnz :: [Int] -> State Reg [Int] -- opcode 3
+jnz literals = do
   state <- get
   if A state == 0:
     then:
-      return
+      jmp 2 
     else:
-      ...
+      jmp (literals !! 1)
 
-bxc :: Int -> State Reg () -- opcode 4
-bxc _ = do
-  state <- get
-  newReg = (B state `xor` C state) -- ? reformat 
-  put (state {B = newReg})
-  return
+bxc :: [Int] -> State Reg [Int] -- opcode 4
+bxc = get >>= (\state -> bx (C state))
 
-out :: Int -> State Reg () -- opcode 5
-out combo = modify {O = combo % 8}
+out :: [Int] -> State Reg [Int] -- opcode 5
+out combos = modify {O = (combos !! 1) % 8} >>=  jmp 2
 
-bdv :: Int -> State Reg () -- opcode 6
+bdv :: [Int] -> State Reg [Int] -- opcode 6
 bdv = dv B
 
-cdv :: Int -> State Reg () -- opcode 7
+cdv :: [Int] -> State Reg [Int] -- opcode 7
 cdv = dv C
 
-dv :: (Reg -> Int) -> (Int -> State Reg ())
-dv getter =  do
+dv :: (Reg -> Int) -> ([Int] -> State Reg [Int])
+dv getter = do
   state <- get
   numerator = A state
   denominator = 2^combo
   newstate = state {getter = numerator / denominator} -- partial function
-  return
+  jmp 2
+
+bx :: Int -> ([Int] -> State Reg [Int])
+bx num = do
+  state <- get
+  newReg = B state `xor` num 
+  put (state {B = newReg})
+  jmp 2
+
+jmp :: Int -> ([Int] -> State Reg [Int]) -- or add to state?
+jmp step = return (drop step list)
 
 data Reg = {
   A :: Int,
