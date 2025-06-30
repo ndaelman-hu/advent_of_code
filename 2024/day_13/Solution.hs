a2572704eb94c8985ccb9b3c73f0891d523f6e20
ndaelman <ndaelman@physik.hu-berlin.de>
2025-06-30 11:39:45 +0200

    - Add correctness check - TODO: Issue is that the solutions are floating point

--- 2024/day_13/Solution.hs
+++ 2024/day_13/Solution.hs
@@ -5,19 +5,22 @@ import Text.Parsec.String
 main :: IO ()
 main = do
   let problems = [
-        (m2 [94, 34, 22, 67], vector [8400, 5400]),
-        (m2 [26, 66, 67, 21], vector [12748, 12176]),
-        (m2 [17, 86, 84, 37], vector [7870, 6450]),
-        (m2 [69, 23, 27, 71], vector [18641, 10279])
+        (m2 [94, 22, 34, 67], vector [8400, 5400]  ),
+        (m2 [26, 67, 66, 21], vector [12748, 12176]),
+        (m2 [17, 84, 86, 37], vector [7870, 6450]  ),
+        (m2 [69, 27, 23, 71], vector [18641, 10279])
         ]
-      solutions = [weights sol | (a, b) <- problems, det a /= 0, let sol = a <\> b]
-  print $ sum solutions
+      solutions = [sol | (a, b) <- problems, det a /= 0, let sol = a <\> b, isClose 1e-5 b (a #> sol)]
+  print solutions
 
 m2 :: [Double] -> Matrix Double
 m2 [a,b,c,d] = (2><2) [a,b,c,d]
 
 weights :: Vector Double -> Double
 weights v = 3 * (v ! 0) + (v ! 1)
+
+isClose :: Double -> Vector Double -> Vector Double -> Bool
+isClose eps v1 v2 = norm_2 (v1 - v2) < eps
 
 -----------------------------
 
