89a28d9348f0b5ec00e16310fcde8398d2270bd8
ndaelman <ndaelman@physik.hu-berlin.de>
2025-07-02 13:48:22 +0200

    - Work out full pipeline - Add input and test files -- TODO fix discrepancies

--- 2024/day_24/Solution.hs
+++ 2024/day_24/Solution.hs
@@ -1,33 +1,65 @@
+import Data.List (sortOn)
 import qualified Data.HashTable.ST.Basic as H
+import Data.HashTable.Class (toList)
 import Control.Monad.ST
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Char (alphaNum, upper, space)
 
 main :: IO ()
-main = return ()
+main = do
+  cvars  <- readFile "vars.txt"
+  cgates <- readFile "gates.txt"
+  let vars  = parse (endBy pvar newline)  "" cvars
+  let gates = parse (endBy pgate newline) "" cgates
+  case (vars, gates) of
+    (Right evars, Right egates) -> putStr . svar . sortOn fst $ solution evars egates
+    _ -> print "Failure"
 
--- solution =  
+solution :: [Var] -> [Gate] -> [Var]
+solution vars gates = runST $ do
+  ht <- H.new
+  mapM_ (uncurry $ H.insert ht) vars 
+  compGates gates ht
+  toList ht
 
-apLogic :: String -> String -> String -> (Bool -> Bool -> Bool) -> H.HashTable s String Bool -> ST s ()
-apLogic lft rght trgt oprtr ht = do
+compGates :: [Gate] -> H.HashTable s String Bool -> ST s ()
+compGates gates ht = do
+  oldLength <- length <$> toList ht
+  mapM_ (`apLogic` ht) gates 
+  newLength <- length <$> toList ht
+  if oldLength == newLength
+     then return ()
+     else compGates gates ht
+
+apLogic :: Gate -> H.HashTable s String Bool -> ST s ()
+apLogic (lft, rght, trgt, oprtr) ht = do
   lb <- H.lookup ht lft
   rb <- H.lookup ht rght
   case (lb, rb) of
     (Just lbb, Just rbb) -> H.insert ht trgt $ oprtr lbb rbb
     (_, _) -> return ()
 
+type Var = (String, Bool)
+type Gate = (String, String, String, Bool -> Bool -> Bool) -- left I, right I, target O
+
 -- parsing
 
-var :: Parser (String, Bool)
-var = do
+svar :: [Var] -> String
+svar = unlines . map stringFromVar
+  where
+    stringFromVar (k, v) = k ++ ": " ++ show (intFromBool v)
+    intFromBool b = if b then 1 else 0
+
+pvar :: Parser Var
+pvar = do
   vname <- count 3 alphaNum
   _     <- string ": "
   val   <- (== '1') <$> oneOf "01"
   return (vname, val)
 
-gate :: Parser (String, String, String, Bool -> Bool -> Bool)
-gate = do
+pgate :: Parser Gate 
+pgate = do
   lname <- count 3 alphaNum
   _     <- space
   op    <- many1 upper
