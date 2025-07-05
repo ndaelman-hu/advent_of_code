ba1152e3f0d56ec8bbea1e27f37bbc6dce2fa37d
ndaelman <ndaelman@physik.hu-berlin.de>
2025-07-05 15:39:12 +0200

    - Add frequency labels - TODO: solSym and fullFilter incorrectly split the Node

--- 2024/day_8/Solution.hs
+++ 2024/day_8/Solution.hs
@@ -3,15 +3,25 @@ import Data.List (nub)
 
 main :: IO ()
 main = do
-  let input = [V2 3 4, V2 4 8, V2 5 5]
-  print $ filter (inBounds $ V2 10 10) . nub . filter (`notElem` input) . foldr (++) [] $ pairAp compAntiNodes input 
+  let inputs = [('a', V2 3 4), ('a', V2 4 8), ('a', V2 5 5)]
+  print . fullFilter inputs $ concat [solSym $ filter (\x -> fst x==k) inputs | k <- nub $ fmap fst inputs] 
+
+fullFilter :: [Node] -> [Node] -> [Node]
+fullFilter ogs ns = zip ks $ filter (inBounds (V2 10 10)) . nub . filter (`notElem` fmap snd ogs) $ vs
+  where (ks, vs) = unzip ns
+
+inBounds :: V2 Int -> V2 Int -> Bool
+inBounds (V2 rx ry) (V2 x y) = x < rx && y < ry 
+
+solSym :: [Node] -> [Node]
+solSym ns = zip ks $ concat $ pairAp compAntiNodes vs
+  where (ks, vs) = unzip ns
 
 pairAp :: Applicative f => (a -> a -> b) -> f a -> f b
 pairAp f x = f <$> x <*> x
 
-inBounds :: V2 Int -> V2 Int -> Bool
-inBounds (V2 rx ry) (V2 x y) = if x < rx && y < ry then True else False 
-
 compAntiNodes :: V2 Int -> V2 Int -> [V2 Int]
 compAntiNodes v w = [v + d, w - d] 
   where d = v - w
+
+type Node = (Char, V2 Int)
