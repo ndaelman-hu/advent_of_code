1415fe1df404fff8d279c40368520e87111196ec
ndaelman <ndaelman@physik.hu-berlin.de>
2025-07-05 14:03:47 +0200

    Add inBounds filter

--- 2024/day_8/Solution.hs
+++ 2024/day_8/Solution.hs
@@ -4,10 +4,13 @@ import Data.List (nub)
 main :: IO ()
 main = do
   let input = [V2 3 4, V2 4 8, V2 5 5]
-  print $ nub . filter (`notElem` input) . foldr (++) [] $ pairAp compAntiNodes input 
+  print $ filter (inBounds $ V2 10 10) . nub . filter (`notElem` input) . foldr (++) [] $ pairAp compAntiNodes input 
 
 pairAp :: Applicative f => (a -> a -> b) -> f a -> f b
 pairAp f x = f <$> x <*> x
+
+inBounds :: V2 Int -> V2 Int -> Bool
+inBounds (V2 rx ry) (V2 x y) = if x < rx && y < ry then True else False 
 
 compAntiNodes :: V2 Int -> V2 Int -> [V2 Int]
 compAntiNodes v w = [v + d, w - d] 
