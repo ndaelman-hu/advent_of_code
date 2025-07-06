54f431b2830a663f4bdec3f5490ac88cbbe299ce
ndaelman <ndaelman@physik.hu-berlin.de>
2025-07-06 12:32:26 +0200

    - Fix logic issue that pre-emptively disconnected keys and positions -- Rework compAntiNodes to accept Node: is this approach inevitable? -- Simplify main statement drastically by removing filtering

--- 2024/day_8/Solution.hs
+++ 2024/day_8/Solution.hs
@@ -4,7 +4,7 @@ import Data.List (nub)
 main :: IO ()
 main = do
   let inputs = [('a', V2 3 4), ('a', V2 4 8), ('a', V2 5 5)]
-  print . fullFilter inputs $ concat [solSym $ filter (\x -> fst x==k) inputs | k <- nub $ fmap fst inputs] 
+  print . fullFilter inputs $ solSymb inputs
 
 fullFilter :: [Node] -> [Node] -> [Node]
 fullFilter ogs ns = zip ks $ filter (inBounds (V2 10 10)) . nub . filter (`notElem` fmap snd ogs) $ vs
@@ -13,15 +13,17 @@ fullFilter ogs ns = zip ks $ filter (inBounds (V2 10 10)) . nub . filter (`notEl
 inBounds :: V2 Int -> V2 Int -> Bool
 inBounds (V2 rx ry) (V2 x y) = x < rx && y < ry 
 
-solSym :: [Node] -> [Node]
-solSym ns = zip ks $ concat $ pairAp compAntiNodes vs
-  where (ks, vs) = unzip ns
+solSymb :: [Node] -> [Node]
+solSymb = concat . pairAp compAntiNodes
 
 pairAp :: Applicative f => (a -> a -> b) -> f a -> f b
 pairAp f x = f <$> x <*> x
 
-compAntiNodes :: V2 Int -> V2 Int -> [V2 Int]
-compAntiNodes v w = [v + d, w - d] 
+compAntiNodes :: Node -> Node -> [Node]
+compAntiNodes (k, v) (l, w) =
+  if k == l
+    then [(k, v + d), (l, w - d)]
+    else []
   where d = v - w
 
 type Node = (Char, V2 Int)
