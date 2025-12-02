3591437389bd49ad1295cca82b3295c6fcd00a6c
ndaelman <ndaelman@physik.hu-berlin.de>
2025-12-02 21:29:10 +0100

    Find solution 2

--- 2025/day_02/solution2/solution.hs
+++ 2025/day_02/solution2/solution.hs
@@ -4,24 +4,30 @@ import Text.Parsec.String (Parser, parseFromFile)
 
 main :: IO ()
 main = do
-  inp <- parseFromFile ranges "input.test.txt"
+  inp <- parseFromFile ranges "input.txt"
   case inp of
     Left e -> print e
-    Right r -> print $ f $ t2l =<< r
-    -- Right r -> print . sum $ f $ t2l =<< r
+    Right r -> print . sum $ f $ t2l =<< r
+    -- Right r -> print $ f $ t2l =<< r
 
 f :: [Int] -> [Int]
-f x = read . snd <$> filter (\(n, s) -> isOfNLength n s && isValidNId n s) y
-  where y = [(i, show x') | i <- [2..maximum (length . show <$> x)], x' <- x]
+f x = read <$> filter isValidNId (show <$> x)
 
-isOfNLength :: Int -> String -> Bool
-isOfNLength n s = mod (length s) n == 0
+isValidNId :: String -> Bool
+isValidNId s = 
+  let factors = uniqueDividersNoPrime (length s)
+      chunks  = fmap (`splitEqChunks` s) factors
+  in not (null factors) && any id (fmap (\x -> all (== head x) (tail x)) chunks)
 
-isValidNId :: Int -> String -> Bool
-isValidNId n s = all (== head xs) (tail xs) 
+splitEqChunks :: Int -> String -> [String] -- requires all dividers, else produces list with the last element of shorter length
+splitEqChunks nchunk s = [take ssize (drop (k*ssize) s) | k <- [0..nchunk-1]]
+  where ssize = length s `div` nchunk
+
+uniqueDividersNoPrime :: Int -> [Int]
+uniqueDividersNoPrime n = fact n [] -- ref factor collection
   where
-    i = length s `div` n -- is ensured by isOfNLength
-    xs = [take i (drop (k*i) s) | k <- [0..n-1]]
+    fact 1 xs = xs -- [n-1..1] excludes primes
+    fact m xs = fact (m-1) (if n `mod` m == 0 then m:xs else xs)
 
 t2l :: (Int, Int) -> [Int]
 t2l (m, n) = [m..n]
