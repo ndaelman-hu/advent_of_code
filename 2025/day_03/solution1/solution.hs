32f2da4751a972f15883a8323f6023694f5be08a
ndaelman <ndaelman@physik.hu-berlin.de>
2025-12-03 15:54:50 +0100

    Apply lenses (NOT YET COMPLETED)

--- 2025/day_03/solution1/solution.hs
+++ 2025/day_03/solution1/solution.hs
@@ -7,44 +7,50 @@ main :: IO ()
 main = print . f $ read <$> "234234234234278"
 
 f :: [Int] -> Selector [Int]
-f xs = (\x -> mapFst show (byMax x)) =<< (slosh byMax Selector hs) <*> Selector ts
+f xs = (\x -> mapFst show (byMax x)) =<< (slosh byMax (Selector hs)) <*> Selector ts
   where 
     byLast xs = (init xs, last xs)
     (hs, ts) = splitl byLast (return xs)
 
 -- Selector definitions
 
-data Selector a = Selector (String, a) {_selStr :: String, _selVal :: a}
+data Selector a = Selector {_selStr :: String, _selVal :: a}
 
 makeLenses ''Selector
 
 instance Functor Selector where
   fmap :: (a -> b) -> Selector a -> Selector b
-  fmap f Selector (s, x) = Selector (s, f x)
+  fmap f sel = sel & selVal %~ f
 
 instance Applicative Selector where
   pure :: a -> Selector a 
-  pure x = Selector ("", x)
+  pure = Selector ""
 
   (<*>) :: Selector (a -> b) -> Selector a -> Selector b
-  Selector (s1, f) <*> Selector (s2, x) = Selector (s1 ++ s2, f x)
+  sel1 <*> sel2 = Selector
+    (sel1 ^. selStr ++ sel2 ^. selStr)
+    (sel1 ^. selVal $ sel2 ^. selVal)
 
 instance Monad Selector where
   return = pure
 
   (>>=) :: Monad m => m a -> (a -> m b) -> m b
-  Selector (s1, x) >>= f = Selector (s1 ++ s2, y)
-    where Selector (s2, y) = f x
+  sel >>= f = Selector (sel ^. selStr ++ selh ^. selStr) (selh ^. selVal)
+      where selh = f $ sel ^. selVal 
 
 -- extra Selector functions
 
 slosh :: ([a] -> (Int, a)) -> Selector [a] -> Selector [a]
-slosh f (Selector (s, xs)) = Selector (s ++ show y, removeAt j xs)
-  where (y, j) = f xs
+slosh f sel = Selector (s ++ show y) (removeAt j xs)
+  where
+    xs = sel ^. selVal
+    (y, j) = f xs
 
 splitl :: ([a] -> ([a], [a])) -> Selector [a] -> (Selector [a], Selector [a])
-splitl f Selector (s, xs) = (Selector (s, ls), Selector ("", rs))
-  where (ls, rs) = f xs
+splitl f sel = (Selector (sel ^. selStr, ls), Selector ("", rs))
+  where
+    xs = sel ^. selVal
+    (ls, rs) = f xs
 
 -- generic list selection
 
