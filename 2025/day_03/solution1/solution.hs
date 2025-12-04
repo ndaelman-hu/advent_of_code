b6b75cada8b4f127fbfd99f667297292193850fd
ndaelman <ndaelman@physik.hu-berlin.de>
2025-12-04 20:53:28 +0100

    Extend to multi-line. TODO: debug logic

--- 2025/day_03/solution1/solution.hs
+++ 2025/day_03/solution1/solution.hs
@@ -1,8 +1,14 @@
 import Data.List (sortBy)
 import Data.Ord (comparing, Down(Down))
+import Text.Parsec
+import Text.Parsec.String (Parser, parseFromFile)
 
 main :: IO ()
-main = print . f $ indexList "234234234234278"
+main = do
+  inp <- parseFromFile rows "input.test.txt"
+  case inp of
+    Left e -> print e
+    Right r -> print $ fmap (f . indexList) r 
 
 f :: IVal a -> [a]
 f (IVal xs) = fst <$> take 2 (filter (\x -> snd x >= firstI ys) ys)
@@ -12,7 +18,7 @@ highestFirst :: IVal a -> IVal a -- type sgn can be relaxed
 highestFirst (IVal xs) = if firstI xs == length xs then IVal (swap12 xs) else IVal xs
 
 swap12 :: [a] -> [a] -- swap first 2 list elements
-swap12 (x:y:zs) = y:x:zs
+swap12 (x:y:zs) = y:x:zs  -- handles also if zs == []
 swap12 xs = xs  -- handles [], [x]
 
 firstI :: [(a, Int)] -> Int 
@@ -22,4 +28,9 @@ indexList :: [a] -> IVal a
 indexList xs = IVal $ zip xs [1..]
 
 newtype IVal a = IVal [(a,Int)]
-  deriving (Eq,Ord)
+  deriving (Eq,Ord,Show)
+
+-- parsing
+
+rows :: Parser [String]
+rows = init <$> sepBy (many digit) newline
