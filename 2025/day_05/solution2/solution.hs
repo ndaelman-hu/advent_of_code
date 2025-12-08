9d1ca8f52b3e580993ff4fa55fd4e70c4c4c0a9e
ndaelman <ndaelman@physik.hu-berlin.de>
2025-12-08 14:35:23 +0100

    Make draft pass without warnings + TODO: debug

--- 2025/day_05/solution2/solution.hs
+++ 2025/day_05/solution2/solution.hs
@@ -14,23 +14,27 @@ f :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
 f xs results = 
   if null xs
      then results
-     else let 
-      (lowest, batch) = pickLowest xs
-      (newLowest, nextBatch) = selectRange lowest batch
-      in case newLowest of
-        Left nL -> f (nL:tail results) nextBatch 
-        Right nL -> f (nL:results) nextBatch
+     else let (lowest, batch) = pickLowest xs
+           in if null batch
+                 then lowest:results
+                 else let (newLowest, nextBatch) = selectRange lowest batch
+                    in case newLowest of
+                      Left nL -> if null results then f [nL] nextBatch else f (nL:tail results) nextBatch
+                      Right nL -> f (nL:results) nextBatch
 
 selectRange :: (Int, Int) -> [(Int, Int)] -> (Either (Int, Int) (Int, Int), [(Int, Int)]) -- assume 1st argument minimum lower-bound
 selectRange (lmin, rmin) xs = 
   if null ys
-     then let (minRange, rrest) = pickLowest xs in (Right minRange, rrest) 
+     then if null xs
+        then error "selectRange: both xs and ys empty"
+        else let (minRange, rrest) = pickLowest xs in (Right minRange, rrest) 
      else let (maxRange, _) = pickLowest ys in  (Left (lmin, snd maxRange), rest)
   where (ys, rest) = partition (\x -> fst x >= rmin) xs
 
 -- Note: head, minimum, minimumBy, etc. all throw errors for empty lists
 
 pickLowest :: [(Int, Int)] -> ((Int, Int), [(Int, Int)]) -- pick highest right-boundary among lowest left boundaries
+pickLowest [] = error "pickLowest: empty list"
 pickLowest ranges = (maximumBy (comparing snd) minVals, rest)
   where
     (minVals, rest) = partition (\x -> fst x == fst minVal) ranges
