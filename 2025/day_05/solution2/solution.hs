4362c34ca9d615ef37518c50a6d0527f0946ea95
ndaelman <ndaelman@physik.hu-berlin.de>
2025-12-08 14:19:42 +0100

    Draft of smart region merging

--- 2025/day_05/solution2/solution.hs
+++ 2025/day_05/solution2/solution.hs
@@ -1,3 +1,5 @@
+import Data.List (minimumBy, maximumBy, partition)
+import Data.Ord (comparing)
 import Text.Parsec
 import Text.Parsec.String (Parser, parseFromFile)
 
@@ -6,26 +8,33 @@ main = do
   inp <- parseFromFile parser "input.test.txt"
   case inp of
     Left e -> print e
-    Right (ranges, xs) -> print . length $ f ranges xs
+    Right (ranges, xs) -> print $ f ranges []
 
-f :: [(Int, Int)] -> [Int]
-f ranges = foldlTuple mergeRanges [] ranges
+f :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+f xs results = 
+  if null xs
+     then results
+     else let 
+      (lowest, batch) = pickLowest xs
+      (newLowest, nextBatch) = selectRange lowest batch
+      in case newLowest of
+        Left nL -> f (nL:tail results) nextBatch 
+        Right nL -> f (nL:results) nextBatch
 
-foldlTuple :: ((a, a) -> (a, a) -> [(a, a)]) -> [(a, a)] -> [(a, a)] -> [(a, a)]
-foldlTuple f acc inp = 
+selectRange :: (Int, Int) -> [(Int, Int)] -> (Either (Int, Int) (Int, Int), [(Int, Int)]) -- assume 1st argument minimum lower-bound
+selectRange (lmin, rmin) xs = 
+  if null ys
+     then let (minRange, rrest) = pickLowest xs in (Right minRange, rrest) 
+     else let (maxRange, _) = pickLowest ys in  (Left (lmin, snd maxRange), rest)
+  where (ys, rest) = partition (\x -> fst x >= rmin) xs
 
-mergeRanges :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
-mergeRanges (la, lb) (ra, rb)
-  | lconsistent && rconsistent && lb < ra = [(la, lb), (ra, rb)]
-  | lconsistent && rconsistent && la > rb = [(ra, rb), (la, lb)]
-  | lconsistent && rconsistent && lb >= ra && la < ra = [(la, rb)]
-  | lconsistent && rconsistent && la >= ra && lb < rb = [(ra, rb)]
-  | lconsistent && rconsistent && lb >= rb && la >= ra = [(ra, lb)]
-  | lconsistent && rconsistent && lb >= rb && la < ra = [(la, lb)]
-  | otherwise = error "Internal ranges unordered"
+-- Note: head, minimum, minimumBy, etc. all throw errors for empty lists
+
+pickLowest :: [(Int, Int)] -> ((Int, Int), [(Int, Int)]) -- pick highest right-boundary among lowest left boundaries
+pickLowest ranges = (maximumBy (comparing snd) minVals, rest)
   where
-    lconsistent = la < lb
-    rconsistent = ra < rb
+    (minVals, rest) = partition (\x -> fst x == fst minVal) ranges
+    minVal = minimumBy (comparing fst) ranges
 
 digits :: Parser Int
 digits = read <$> many digit
